72ce1e6a85cf86b3dc584e6240d30f9e9f360d97
Seif-01 <sifreyassin.com>
2023-02-23 12:14:13 -0800

    task 0 update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - Test function for positive or negative
 * Return: 0
 */
 
@@ -9,7 +9,7 @@ int main(void)
 {
 int i;
 
-i = 98;
+i = 0;
 positive_or_negative(i);
 
 return (0);
