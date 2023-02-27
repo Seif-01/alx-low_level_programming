38019bad59592aa8f59c0c407b0d1a36d5cb2c23
Yassine Sifar01~ <sifreyassin@gmail.com>
2023-02-27 20:18:56 +0100

    task 0.1.2.3.4.5 + Readm.md + Main.h

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 /**
 *  * swap_int - swaps the values of two integers
 *   * @a: integer to swap
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 /**
 *  * _strlen - returns the length of a string
 *   * @s: string
