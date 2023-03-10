cbd2d7c277c1b511e3ff0438498397d1c0b13bb8
Seif-01 <sifreyassin.com>
2023-03-10 14:32:19 -0800

    3-puts

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,16 +1,11 @@
 #include "main.h"
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-void _puts(char *str)
-
-/**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
+void _puts(char *s)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+puts(s);
 }
 
