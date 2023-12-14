050263b3c5577d6ce81d68f4dad04cbfcb568ebc
Seif-01 <sifreyassin@gmail.com>
2023-12-14 21:21:13 +0100

    task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(1, quo, 59);
 	return (1);
 }
