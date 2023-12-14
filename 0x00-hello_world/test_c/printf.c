09c1b22799191ee6253df3bea749ccbb631a3a8b
Seif-01 <sifreyassin@gmail.com>
2023-12-14 18:00:43 +0100

    task 2

---
+++ 0x00-hello_world/test_c/printf.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+	int age = 24;
+	printf("My age was %d\n",age);
+	return 0;
+}
---
+++ 0x00-hello_world/test_c/putchar.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+	putchar('S');
+	putchar('F');
+	return 0;
+}
