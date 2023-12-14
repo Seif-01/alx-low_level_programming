66567205cc19d5362fd6ae9359763e0e93ea317c
Seif-01 <sifreyassin@gmail.com>
2023-12-14 19:14:35 +0100

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints a specific message
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
