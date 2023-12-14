6ad447e946e2460cdeab9a5e619b5ca605f25a26
Seif-01 <sifreyassin@gmail.com>
2023-12-14 21:26:50 +0100

    task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -10,6 +10,6 @@ int main(void)
 {
 char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-write(1, quo, 59);
+write(1, quo, 58);
 return (1);
-}i
+}
