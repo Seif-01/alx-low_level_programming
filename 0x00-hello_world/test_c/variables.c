b31ff1ae230c266bc4b4e31aa784737522cd8d26
Seif-01 <sifreyassin@gmail.com>
2023-12-14 18:48:07 +0100

    variables

---
+++ 0x00-hello_world/test_c/variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * 
+ * main - this declare and intialize variables
+ * Return : This returns 0
+ */
+ 
+ int main(void)
+{
+	int x ; /* intialization */
+	x = 50 ; /* declaration */
+	
+	int y = 70 ;      /* intialization + declaration */
+	float z = 56.21 ; /* declare floating number */
+	char A = 'n' ;    /* declare a single character */
+	char name[] = "seif walker" ;  /* declare an array chracter */
+	return (0) ;
+}
