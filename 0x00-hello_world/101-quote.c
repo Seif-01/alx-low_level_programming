856667fa8a75413b0e543c294960f7fd65cd81b8
Seif-01 <sifreyassin@gmail.com>
2023-12-14 21:15:18 +0100

    task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - entry point
+ * description: simple operating system
+ * return: 1
+ */
+
+int main(void)
+{
+char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 58);
+	return(1);
+}
