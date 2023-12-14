0b052682e0e4b74453661e0dc21ee2a2afa0f682
Seif-01 <sifreyassin@gmail.com>
2023-12-14 19:46:16 +0100

    task 4

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print size various type of compiles
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+printf("Size of a char: %c", sizeof(char));
+printf("Size of an int: %i", sizeof(int));
+printf("Size of a long int: %li", sizeof(long int));
+printf("Size of a long long int: %lli", sizeof(long long int));
+printf("Size of a float: %f", sizeof(float));
+return (0);
+}
