29ad8be1afc52d357c509b3a5dfe6bb1708c982a
Seif-01 <sifreyassin@gmail.com>
2023-12-15 20:03:49 +0100

    if else

---
+++ 0x01-variables_if_else_while/testc/if-else.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point if else
+ * Description: if else
+ * Return: 0
+ */
+
+int main(void)
+
+{ int x = 1;
+if
+(x = 0)
+{
+printf("i do not exist");
+} else { printf("i exist");
+}
+return(0);
+}
