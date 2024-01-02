efca650e506f389d4ba89c698f2308863e523b68
Seif-01 <sifreyassin@gmail.com>
2024-01-02 15:03:21 +0100

    pointer update

--- 0x05-pointers_arrays_strings/test/pointer.c
+++ 0x05-pointers_arrays_strings/test/pointer.c
@@ -3,13 +3,14 @@
 void func1(){
 
 int var = 20;
+int var2 = 10;
 printf("var valus is = %d \n",var);
 
 int* ptr;
 
-ptr = &var;
+ptr = &var2;
 
-printf("value of var = %d \n", var);
+printf("value of var = %d \n", var2);
 printf("value of ptr = %p\n", ptr);
 printf("value of *ptr = %d\n",*ptr);
     
@@ -17,5 +18,4 @@ printf("value of *ptr = %d\n",*ptr);
 int main(){
     func1();
     return 0;
-
 } 
