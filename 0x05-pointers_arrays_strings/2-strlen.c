9943a9d55a2e25a9668fa9efa7981bd04f1a483d
Seif-01 <sifreyassin@gmail.com>
2024-01-02 18:33:02 +0100

    test 1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+ #include "main.h"
+ /**
+* x - returns the length of a string
+* @s: string
+* Return: length
+*/
+int _strlen(char *s);
+{
+int x ;
+for (x = 0 ; *s != '\0' ; s++ )
+++x;
+return 0;
+}
