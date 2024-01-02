dc6dc0de533ed07de4415661f13e46accada5817
Seif-01 <sifreyassin@gmail.com>
2024-01-02 14:50:02 +0100

    pointers value

---
+++ 0x05-pointers_arrays_strings/test/pointer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void func1(){
+
+int var = 20;
+printf("var valus is = %d \n",var);
+
+int* ptr;
+
+ptr = &var;
+
+printf("value of var = %d \n", var);
+printf("value of ptr = %p\n", ptr);
+printf("value of *ptr = %d\n",*ptr);
+    
+}
+int main(){
+    func1();
+    return 0;
+}
