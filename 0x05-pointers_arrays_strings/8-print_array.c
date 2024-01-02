e60a71ae7ec22f594b4797a739f91201cea2e7ed
Seif-01 <sifreyassin@gmail.com>
2024-01-02 18:52:53 +0100

    Print Array Elements (rebuild -edit 8)

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,15 +8,16 @@
 */
 void print_array(int *a, int n)
 {
-int i;
+ if (n <= 0) {
+        printf("\n");
+        return;
+    }
 
-for (i = 0; i < (n - 1); i++)
-{
-printf("%d, ", a[i]);
-}
-if (i == (n - 1))
-{
-printf("%d", a[n - 1]);
-}
-printf("\n");
+    printf("%d", a[0]);
+
+    for (int i = 1; i < n; i++) {
+        printf(", %d", a[i]);
+    }
+
+    printf("\n");
 }
